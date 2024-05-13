34daf602bc8dc17dc946bdd8416339acda13350c
rudrapatil04 <01fe22bca037>
2024-05-14 01:11:51 +0530

    odd

---
+++ odd.java
@@ -0,0 +1,16 @@
+public class odd{
+    public static void main(String[] args) {
+        int limit = 20; // Change this to set the limit
+        System.out.println("Odd numbers from 1 to " + limit + ":");
+        for (int i = 1; i <= limit; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
+
+
